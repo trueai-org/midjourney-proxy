4d69ee2bb3e9851ffc2a994b3a56513f6a0ef40b
trueai-org <circle@trueai.org>
2024-07-18 11:45:29 +0800

    v2.2.18

--- src/Midjourney.Infrastructure/GlobalConfiguration.cs
+++ src/Midjourney.Infrastructure/GlobalConfiguration.cs
@@ -15,7 +15,7 @@ namespace Midjourney.Infrastructure
         /// <summary>
         /// 版本号
         /// </summary>
-        public static string Version { get; set; } = "v2.2.17";
+        public static string Version { get; set; } = "v2.2.18";
 
         /// <summary>
         /// 判断是否是 Windows 系统
