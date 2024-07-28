fb55d249b6bf0abc3b9458229ea7e3dba2f2fa01
dbccccccc <dbcloud@yeah.net>
2024-07-28 15:50:35 +0800

    Update linux_install.sh

--- scripts/linux_install.sh
+++ scripts/linux_install.sh
@@ -7,62 +7,66 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-# 配置文件存放目录
-CONFIG_DIR="config_files"
-BASE_DIR=$(pwd)
+# GitHub URL for latest release
+API_URL="https://api.github.com/repos/trueai-org/midjourney-proxy/releases/latest"
 
-function __init__() {
-    ARCH=$(uname -m)
-    if [ "$ARCH" == "x86_64" ]; then
-        ARCH="x64"
-    elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
-        ARCH="arm64"
-    else
-        echo -e "${RED}Unsupported architecture: $ARCH${NC}"
-        exit 1
-    fi
+# 获取系统架构
+ARCH=$(uname -m)
 
-    # 检查包管理器类型
-    if command -v apt-get &>/dev/null; then
-        PKG_MANAGER="apt-get"
-    elif command -v yum &>/dev/null; then
-        PKG_MANAGER="yum"
-    elif command -v dnf &>/dev/null; then
-        PKG_MANAGER="dnf"
-    else
-        echo -e "${RED}No supported package manager found (apt-get, yum, dnf).${NC}"
-        exit 1
-    fi
+# 目录位置
+readonly BASE_DIR=$(pwd)
+readonly CONFIG_DIR="$BASE_DIR/config"
 
-    # 检查是否安装了 curl 和 jq
-    if ! command -v curl &>/dev/null; then
-        echo -e "${YELLOW}curl is required but not installed. Installing curl...${NC}"
-        sudo $PKG_MANAGER update && sudo $PKG_MANAGER install curl -y
-    fi
+# ------------INIT-------------
+# 检查系统架构
+if [ "$ARCH" == "x86_64" ]; then
+    ARCH="x64"
+elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
+    ARCH="arm64"
+else
+    echo -e "${RED}Unsupported architecture: $ARCH${NC}"
+    exit 1
+fi
 
-    if ! command -v jq &>/dev/null; then
-        echo -e "${YELLOW}jq is required but not installed. Installing jq...${NC}"
-        sudo $PKG_MANAGER update && sudo $PKG_MANAGER install jq -y
-    fi
+# 检查包管理器类型
+if command -v apt-get &>/dev/null; then
+    PKG_MANAGER="apt-get"
+elif command -v yum &>/dev/null; then
+    PKG_MANAGER="yum"
+elif command -v dnf &>/dev/null; then
+    PKG_MANAGER="dnf"
+else
+    echo -e "${RED}No supported package manager found (apt-get, yum, dnf).${NC}"
+    exit 1
+fi
 
-    # 检查 curl 和 jq 是否成功安装
-    if ! command -v curl &>/dev/null; then
-        echo -e "${RED}Failed to install curl. Please install it manually.${NC}"
-        exit 1
-    fi
+# 检查是否安装了 curl 和 jq
+if ! command -v curl &>/dev/null; then
+    echo -e "${YELLOW}curl is required but not installed. Installing curl...${NC}"
+    sudo $PKG_MANAGER update && sudo $PKG_MANAGER install curl -y
+fi
 
-    if ! command -v jq &>/dev/null; then
-        echo -e "${RED}Failed to install jq. Please install it manually.${NC}"
-        exit 1
-    fi
+if ! command -v jq &>/dev/null; then
+    echo -e "${YELLOW}jq is required but not installed. Installing jq...${NC}"
+    sudo $PKG_MANAGER update && sudo $PKG_MANAGER install jq -y
+fi
 
-    # 配置文件目录
-    if [ ! -d "$CONFIG_DIR" ]; then
-        mkdir -p "$CONFIG_DIR"
-        echo -e "${GREEN}Configuration directory created: $CONFIG_DIR${NC}"
-    fi
-}
+# 检查 curl 和 jq 是否成功安装
+if ! command -v curl &>/dev/null; then
+    echo -e "${RED}Failed to install curl. Please install it manually.${NC}"
+    exit 1
+fi
 
+if ! command -v jq &>/dev/null; then
+    echo -e "${RED}Failed to install jq. Please install it manually.${NC}"
+    exit 1
+fi
+
+# 配置文件目录
+if [ ! -d "$CONFIG_DIR" ]; then
+    mkdir -p "$CONFIG_DIR"
+    echo -e "${GREEN}Configuration directory created: $CONFIG_DIR${NC}"
+fi
 
 # 下载文件函数，包含镜像重试逻辑
 function download_file() {
@@ -74,7 +78,7 @@ function download_file() {
     if [ $? -ne 0 ]; then
         echo -e "${RED}Failed to download $url.${NC}"
         echo -e "${YELLOW}Switching to proxy and retrying...${NC}"
-        url="https://mirror.ghproxy.com/${url#https://}"
+        url="https://mirror.ghproxy.com/${url}"
         curl -L -o "$output" "$url"
         if [ $? -ne 0 ]; then
             echo -e "${RED}Failed to download $url using proxy. Please check your network and try again.${NC}"
@@ -82,9 +86,6 @@ function download_file() {
         fi
     fi
 }
-
-# GitHub API URL for latest release
-API_URL="https://api.github.com/repos/trueai-org/midjourney-proxy/releases/latest"
 
 # 获取最新版本的下载链接
 function get_latest_version_info() {
@@ -99,113 +100,22 @@ function get_latest_version_info() {
     RESPONSE=$(fetch_version_info)
     if [ $? -ne 0 ]; then
         echo -e "${YELLOW}Failed to retrieve version info. Switching to proxy...${NC}"
-        api_url="https://mirror.ghproxy.com/${API_URL#https://}"
+        api_url="https://mirror.ghproxy.com/${API_URL}"
         RESPONSE=$(fetch_version_info)
         if [ $? -ne 0 ]; then
             echo -e "${RED}Failed to retrieve version info using proxy. Please check your network and try again.${NC}"
-            exit 1
-        fi
-    fi
+            # exit 1
+        else
+            LATEST_VERSION=$(echo $RESPONSE | jq -r '.tag_name')
+            DOWNLOAD_URL=$(echo $RESPONSE | jq -r ".assets[] | select(.name | test(\"midjourney-proxy-linux-${ARCH}\")) | .browser_download_url")
 
-    LATEST_VERSION=$(echo $RESPONSE | jq -r '.tag_name')
-    DOWNLOAD_URL=$(echo $RESPONSE | jq -r ".assets[] | select(.name | test(\"midjourney-proxy-linux-${ARCH}\")) | .browser_download_url")
-
-    if [ -n "$LATEST_VERSION" ] && [ -n "$DOWNLOAD_URL" ]; then
-        LATEST_TAR_URL=$DOWNLOAD_URL
-    else
-        echo -e "${RED}Failed to retrieve the latest version information.${NC}"
-        exit 1
-    fi
-}
-
-# 列出配置文件
-function list_config_files() {
-    CONFIG_FILES=()
-    echo -e "${BLUE}Configuration files:${NC}"
-    local index=1
-    for config_file in "$CONFIG_DIR"/*; do
-        if [ -f "$config_file" ]; then
-            CONFIG_FILES+=("$(basename "$config_file")")
-            echo -e "  $index) ${CONFIG_FILES[-1]}"
-            index=$((index + 1))
-        fi
-    done
-
-    if [ ${#CONFIG_FILES[@]} -eq 0 ]; then
-        echo -e "${YELLOW}No configuration files found.${NC}"
-        return 1
-    else
-        return 0
-    fi
-}
-
-# 根据用户选择的编号获取配置文件名称
-function get_config_file_by_number() {
-    local num=$1
-    if ((num > 0 && num <= ${#CONFIG_FILES[@]})); then
-        echo "${CONFIG_FILES[$((num - 1))]}"
-    else
-        echo ""
-    fi
-}
-
-# 重命名配置文件
-function rename_config_file() {
-    if list_config_files; then
-        read -p "Select configuration file to rename by number: " num
-        selected_file=$(get_config_file_by_number $num)
-        if [ -n "$selected_file" ]; then
-            read -p "Enter the new configuration file name: " NEW_NAME
-            # 检查新名称是否为空或已存在
-            if [ -z "$NEW_NAME" ] || [ -e "$CONFIG_DIR/$NEW_NAME" ]; then
-                echo -e "${RED}Invalid new name or file already exists.${NC}"
+            if [ -n "$LATEST_VERSION" ] && [ -n "$DOWNLOAD_URL" ]; then
+                LATEST_TAR_URL=$DOWNLOAD_URL
             else
-                mv "$CONFIG_DIR/$selected_file" "$CONFIG_DIR/$NEW_NAME"
-                echo -e "${GREEN}Configuration file renamed to: $NEW_NAME${NC}"
+                echo -e "${RED}Failed to retrieve the latest version information.${NC}"
+                exit 1
             fi
-        else
-            echo -e "${RED}Invalid selection.${NC}"
         fi
-    fi
-}
-
-# 删除配置文件
-function delete_config_file() {
-    if list_config_files; then
-        read -p "Select configuration file to delete by number: " num
-        selected_file=$(get_config_file_by_number $num)
-        if [ -n "$selected_file" ]; then
-            read -p "Are you sure you want to delete $selected_file? [y/N]: " CONFIRM
-            CONFIRM=$(echo "$CONFIRM" | tr '[:upper:]' '[:lower:]')
-            if [ "$CONFIRM" == "y" ]; then
-                rm "$CONFIG_DIR/$selected_file"
-                echo -e "${GREEN}Configuration file deleted: $selected_file${NC}"
-            else
-                echo -e "${YELLOW}Deletion canceled.${NC}"
-            fi
-        else
-            echo -e "${RED}Invalid selection.${NC}"
-        fi
-    fi
-}
-
-# 从指定版本导出配置文件
-function export_config_file() {
-    list_installed_versions
-    read -p "Enter the version to export configuration from (e.g., v2.3.7): " VERSION
-    if [ -d "$VERSION" ]; then
-        read -p "Enter the name for the exported configuration file: " EXPORT_NAME
-        # 删除用户输入的名字中的 `.json` 后缀
-        EXPORT_NAME="${EXPORT_NAME%.json}"
-        # 检查新名称是否为空或已存在
-        if [ -z "$EXPORT_NAME" ] || [ -e "$CONFIG_DIR/$EXPORT_NAME.json" ]; then
-            echo -e "${RED}Invalid new name or file already exists.${NC}"
-        else
-            cp "$VERSION/appsettings.json" "$CONFIG_DIR/$EXPORT_NAME.json"
-            echo -e "${GREEN}Configuration file exported as: $EXPORT_NAME${NC}"
-        fi
-    else
-        echo -e "${RED}Version directory not found: $VERSION${NC}"
     fi
 }
 
@@ -542,18 +452,6 @@ function is_latest_version_installed() {
     return 1 # 未安装
 }
 
-# 找到本地最新版本
-function find_local_latest_version() {
-    local latest_version="0.0.0"
-    for version_dir in v*; do
-        version=$(echo $version_dir | sed 's/^v//')
-        if dpkg --compare-versions "$version" "gt" "$latest_version"; then
-            latest_version=$version
-        fi
-    done
-    echo "v$latest_version"
-}
-
 # 删除版本并安装新版本
 function delete_version_and_install() {
     VERSION=$1
@@ -633,7 +531,7 @@ init_config_dir
 # 获取最新版本信息
 get_latest_version_info
 
-until [ "$OPTION" == "7" ]; do
+until [ "$OPTION" == "6" ]; do
     echo
     check_running_version # 调用检查正在运行的版本函数
     list_installed_versions
@@ -642,7 +540,6 @@ until [ "$OPTION" == "7" ]; do
     echo -e "1. ${GREEN}Install or update to the latest version ($LATEST_VERSION)${NC}"
     echo -e "2. ${GREEN}Install a specific version${NC}"
     echo -e "3. ${GREEN}Delete a specific version${NC}"
-    echo -e "4. ${GREEN}Manage configuration files${NC}"
     echo -e "5. ${GREEN}Start a specific version${NC}"
     echo -e "6. ${GREEN}Stop running version${NC}" # 新增加的选项
     echo -e "7. ${GREEN}Exit${NC}"                 # 更新退出选项的编号
@@ -659,41 +556,7 @@ until [ "$OPTION" == "7" ]; do
     3)
         delete_version
         ;;
-    4)
-        CONFIG_OPTION=""
-        until [ "$CONFIG_OPTION" == "6" ]; do
-            echo "Configuration File Management:"
-            echo -e "1. ${GREEN}List configuration files${NC}"
-            echo -e "2. ${GREEN}Rename a configuration file${NC}"
-            echo -e "3. ${GREEN}Delete a configuration file${NC}"
-            echo -e "4. ${GREEN}Export configuration from version${NC}"
-            echo -e "5. ${GREEN}Apply configuration to version${NC}"
-            echo -e "6. ${GREEN}Back to main menu${NC}"
-            read -p "Choose an option (1/2/3/4/5/6): " CONFIG_OPTION
-
-            case $CONFIG_OPTION in
-            1)
-                list_config_files
-                ;;
-            2)
-                rename_config_file
-                ;;
-            3)
-                delete_config_file
-                ;;
-            4)
-                export_config_file
-                ;;
-            5)
-                apply_config_to_version
-                ;;
-            6) ;;
-            *)
-                echo -e "${RED}Invalid option.${NC}"
-                ;;
-            esac
-        done
-        ;;
+    4) ;;
     5)
         start_program_version
         ;;
