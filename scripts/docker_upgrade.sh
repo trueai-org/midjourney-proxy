fd308fb69b1ac0f397379a634bf050ceb70cdef6
trueai-org <circle@trueai.org>
2024-07-31 14:23:48 +0800

    upgrade sh

---
+++ scripts/docker_upgrade.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# 定义一些变量
+IMAGE_NAME="registry.cn-guangzhou.aliyuncs.com/trueai-org/midjourney-proxy"
+CONTAINER_NAME="mjopen"
+
+# 打印信息
+echo "开始更新 ${CONTAINER_NAME} 容器..."
+
+# 验证Docker是否安装
+if ! command -v docker &> /dev/null
+then
+    echo "Docker 未安装，请先安装 Docker。"
+    exit 1
+fi
+
+# 拉取最新镜像
+echo "拉取最新的镜像 ${IMAGE_NAME}..."
+docker pull ${IMAGE_NAME}
+if [ $? -ne 0 ]; then
+    echo "拉取镜像失败，请检查网络连接或镜像地址是否正确。"
+    exit 1
+fi
+
+# 停止并移除现有容器
+if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
+    echo "停止现有的容器 ${CONTAINER_NAME}..."
+    docker stop ${CONTAINER_NAME}
+    if [ $? -ne 0 ]; then
+        echo "停止容器失败，请手动检查。"
+        exit 1
+    fi
+fi
+
+if [ "$(docker ps -aq -f status=exited -f name=${CONTAINER_NAME})" ]; then
+    echo "移除现有的容器 ${CONTAINER_NAME}..."
+    docker rm ${CONTAINER_NAME}
+    if [ $? -ne 0 ]; then
+        echo "移除容器失败，请手动检查。"
+        exit 1
+    fi
+fi
+
+# 运行新的容器
+echo "启动新的容器 ${CONTAINER_NAME}..."
+docker run -m 1g --name ${CONTAINER_NAME} -d --restart=always \
+ -e DEMO=true \
+ -p 8086:8080 --user root \
+ -v /root/mjopen/logs:/app/logs:rw \
+ -v /root/mjopen/data:/app/data:rw \
+ -v /root/mjopen/attachments:/app/wwwroot/attachments:rw \
+ -v /root/mjopen/ephemeral-attachments:/app/wwwroot/ephemeral-attachments:rw \
+ -v /root/mjopen/appsettings.Production.json:/app/appsettings.Production.json:ro \
+ -e TZ=Asia/Shanghai \
+ -v /etc/localtime:/etc/localtime:ro \
+ -v /etc/timezone:/etc/timezone:ro \
+ ${IMAGE_NAME}
+if [ $? -ne 0 ]; then
+    echo "启动新的容器失败，请手动检查。"
+    exit 1
+fi
+
+echo "容器 ${CONTAINER_NAME} 更新并启动成功！"
+
