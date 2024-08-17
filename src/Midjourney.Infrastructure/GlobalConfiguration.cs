9998023e8ea990bbf3448419b21c00c116870e22
trueai-org <circle@trueai.org>
2024-08-17 17:48:43 +0800

    v3.9.6

--- src/Midjourney.Infrastructure/GlobalConfiguration.cs
+++ src/Midjourney.Infrastructure/GlobalConfiguration.cs
@@ -15,7 +15,7 @@ namespace Midjourney.Infrastructure
         /// <summary>
         /// 版本号
         /// </summary>
-        public static string Version { get; set; } = "v3.9.5";
+        public static string Version { get; set; } = "v3.9.6";
 
         /// <summary>
         /// 全局配置项
