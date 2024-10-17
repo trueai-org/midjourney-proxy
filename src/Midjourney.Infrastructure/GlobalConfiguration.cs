55db70d2e0b7e51c47bfdb2175f921953d21d860
trueai-org <circle@trueai.org>
2024-10-17 22:25:35 +0800

    v5.9.3

--- src/Midjourney.Infrastructure/GlobalConfiguration.cs
+++ src/Midjourney.Infrastructure/GlobalConfiguration.cs
@@ -40,7 +40,7 @@ namespace Midjourney.Infrastructure
         /// <summary>
         /// 版本号
         /// </summary>
-        public static string Version { get; set; } = "v5.9.2";
+        public static string Version { get; set; } = "v5.9.3";
 
         /// <summary>
         /// 全局配置项
