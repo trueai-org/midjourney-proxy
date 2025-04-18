47889c24866481d0a1da6cf1b620924f43aa93b5
trueai-org <circle@trueai.org>
2025-04-18 12:35:52 +0800

    v6.7.0

--- src/Midjourney.Infrastructure/Services/BestWaitIdleRule.cs
+++ src/Midjourney.Infrastructure/Services/BestWaitIdleRule.cs
@@ -32,45 +32,88 @@ namespace Midjourney.Infrastructure.LoadBalancer
     {
         private static readonly Random random = new Random();
 
+        ///// <summary>
+        ///// 根据最少等待空闲规则选择一个 Discord 实例
+        ///// </summary>
+        ///// <param name="instances">可用的 Discord 实例列表。</param>
+        ///// <returns>选择的 Discord 实例。</returns>
+        //public DiscordInstance Choose(List<DiscordInstance> instances)
+        //{
+        //    if (instances.Count == 0)
+        //    {
+        //        return null;
+        //    }
+
+        //    // FIX：此算法存在问题：因为可能没有执行中的任务，由于间隔较大时，那么会一直选择到同一个实例
+        //    //// 优先选择空闲的实例
+        //    //var idleCandidates = instances
+        //    //    .Where(c => c.Account.CoreSize - c.GetRunningFutures().Count > 0)
+        //    //    .GroupBy(c => c.Account.CoreSize - c.GetRunningFutures().Count)
+        //    //    .OrderByDescending(g => g.Key)
+        //    //    .FirstOrDefault();
+
+        //    //if (idleCandidates != null)
+        //    //{
+        //    //    // 随机选择一个空闲实例
+        //    //    return idleCandidates.ElementAt(random.Next(idleCandidates.Count()));
+        //    //}
+
+        //    // 如果没有空闲的实例，则选择 -> (当前队列数 + 执行中的数量) / 核心数, 最小的实例
+        //    var busyCandidates = instances
+        //        .GroupBy(c => (double)(c.GetRunningFutures().Count + c.GetQueueTasks().Count) / c.Account.CoreSize)
+        //        .OrderBy(g => g.Key)
+        //        .FirstOrDefault();
+
+        //    if (busyCandidates != null)
+        //    {
+        //        // 随机选择一个实例
+        //        return busyCandidates.ElementAt(random.Next(busyCandidates.Count()));
+        //    }
+
+        //    return null;
+        //}
+
         /// <summary>
-        /// 根据最少等待空闲规则选择一个 Discord 实例
+        /// 根据队列利用率选择一个Discord实例
         /// </summary>
-        /// <param name="instances">可用的 Discord 实例列表。</param>
-        /// <returns>选择的 Discord 实例。</returns>
+        /// <param name="instances">可用的Discord实例列表</param>
+        /// <returns>选择的Discord实例</returns>
         public DiscordInstance Choose(List<DiscordInstance> instances)
         {
-            if (instances.Count == 0)
+            if (instances == null || instances.Count == 0)
             {
                 return null;
             }
 
-            // FIX：此算法存在问题：因为可能没有执行中的任务，由于间隔较大时，那么会一直选择到同一个实例
-            //// 优先选择空闲的实例
-            //var idleCandidates = instances
-            //    .Where(c => c.Account.CoreSize - c.GetRunningFutures().Count > 0)
-            //    .GroupBy(c => c.Account.CoreSize - c.GetRunningFutures().Count)
-            //    .OrderByDescending(g => g.Key)
-            //    .FirstOrDefault();
-
-            //if (idleCandidates != null)
-            //{
-            //    // 随机选择一个空闲实例
-            //    return idleCandidates.ElementAt(random.Next(idleCandidates.Count()));
-            //}
-
-            // 如果没有空闲的实例，则选择 -> (当前队列数 + 执行中的数量) / 核心数, 最小的实例
-            var busyCandidates = instances
-                .GroupBy(c => (double)(c.GetRunningFutures().Count + c.GetQueueTasks().Count) / c.Account.CoreSize)
-                .OrderBy(g => g.Key)
-                .FirstOrDefault();
-
-            if (busyCandidates != null)
+            // 计算每个实例的队列利用情况
+            var instanceMetrics = instances.Select(instance =>
             {
-                // 随机选择一个实例
-                return busyCandidates.ElementAt(random.Next(busyCandidates.Count()));
-            }
+                int queuedTasks = instance.GetQueueTasks().Count;
+                int queueSize = instance.Account.QueueSize;
 
-            return null;
+                // 计算队列利用率
+                double queueUtilization = queueSize > 0 ? (double)queuedTasks / queueSize : 1.0;
+
+                // 计算队列剩余空间
+                int remainingQueueSpace = Math.Max(0, queueSize - queuedTasks);
+
+                return new
+                {
+                    Instance = instance,
+                    QueueUtilization = queueUtilization,
+                    RemainingQueueSpace = remainingQueueSpace
+                };
+            }).ToList();
+
+            // 按队列利用率分组，选择利用率最低的组
+            var bestGroup = instanceMetrics
+                .GroupBy(m => m.QueueUtilization)
+                .OrderBy(g => g.Key)  // 队列利用率越低越好
+                .First();
+
+            // 如果有多个实例具有相同的最低队列利用率，随机选择一个
+            int randomIndex = random.Next(bestGroup.Count());
+            return bestGroup.ElementAt(randomIndex).Instance;
         }
     }
 
