8762de0f97c397f72e964e219ca23d95b24c7a57
trueai-org <circle@trueai.org>
2026-04-02 23:10:06 +0800

    BANNED_WORDS

--- src/Midjourney.Base/Utils/MjBannedWordsHelper.cs
+++ src/Midjourney.Base/Utils/MjBannedWordsHelper.cs
@@ -33,217 +33,111 @@ namespace Midjourney.Base.Util
         /// 禁用词列表
         /// </summary>
         private const string BANNED_WORDS = """
-            blood
-            twerk
-            making love
-            voluptuous
-            naughty
-            wincest
-            orgy
-            no clothes
-            au naturel
-            no shirt
-            decapitate
-            bare
-            nude
+            ahegao
+            anus
+            ass
+            ballgag
             barely dressed
-            nude
-            bra
-            risque
-            scantily clad
-            cleavage
-            stripped
-            infested
-            full frontal
-            unclothed
-            invisible clothes
-            wearing nothing
-            lingerie
-            with no shirt
-            naked
-            without clothes on
-            negligee
-            zero clothes
-            gruesome
-            fascist
-            nazi
-            prophet mohammed
-            slave
-            coon
-            honkey
-            cocaine
-            heroin
-            meth
-            crack
-            kill
-            belle delphine
-            hitler
-            jinping
-            lolita
-            president xi
-            torture
-            disturbing
-            farts
-            fart
-            poop
-            infected
-            warts
-            shit
-            brown pudding
-            bunghole
-            vomit
-            voluptuous
-            seductive
-            sperm
-            sexy
-            sadist
-            sensored
-            censored
-            silenced
-            deepfake
-            inappropriate
-            waifu
-            succubus
-            slaughter
-            surgery
-            reproduce
-            crucified
-            seductively
-            explicit
-            inappropriate
-            large bust
-            explicit
-            wang
-            inappropriate
-            teratoma
-            intimate
-            see through
-            tryphophobia
             bloodbath
-            wound
-            cronenberg
-            khorne
+            bloody
+            bodily fluids
+            boobs
+            breasts
+            brothel
+            brown pudding
+            bruises
+            bunghole
             cannibal
             cannibalism
-            visceral
-            guts
-            bloodshot
-            gory
-            killing
-            crucifixion
-            surgery
-            vivisection
-            massacre
-            hemoglobin
-            suicide
-            arse
-            labia
-            ass
-            mammaries
-            badonkers
-            bloody
-            minge
-            big ass
-            mommy milker
-            booba
-            nipple
-            oppai
-            booty
-            organs
-            bosom
-            ovaries
-            flesh
-            breasts
-            penis
-            busty
-            phallus
-            clunge
-            sexy female
-            crotch
-            skimpy
-            dick
-            thick
-            bruises
-            girth
-            titty
-            honkers
-            vagina
-            hooters
-            veiny
-            knob
-            ahegao
-            pinup
-            ballgag
             car crash
-            playboy
-            bimbo
-            pleasure
-            bodily fluids
-            pleasures
-            boudoir
-            rule34
-            brothel
-            seducing
-            dominatrix
+            cleavage
+            clunge
+            cocaine
+            coon
             corpse
-            seductive
+            crack
+            crotch
+            crucifixion
+            decapitate
+            deepfake
+            dick
+            dominatrix
             erotic
-            seductive
-            fuck
-            sensual
-            hardcore
-            sexy
-            hentai
-            shag
-            horny
-            crucified
-            shibari
-            incest
-            smut
-            jav
-            succubus
-            jerk off king at pic
-            thot
-            kinbaku
-            legs spread
-            sensuality
-            belly button
-            porn
-            patriotic
-            bleed
             excrement
-            petite
-            seduction
-            mccurry
-            provocative
-            sultry
-            erected
-            camisole
-            tight white
-            arrest
-            see-through
+            explicit
+            fart
+            farts
             feces
-            anus
-            revealing clothing
-            vein
+            flesh
+            fuck
+            full frontal
+            gore
+            gory
+            gruesome
+            guts
+            hardcore
+            hentai
+            heroin
+            hitler
+            honkey
+            incest
+            infected
+            intimate parts
+            jav
+            kill
+            killing
+            kinbaku
+            labia
+            legs spread
+            lingerie
             loli
-            -edge
-            boobs
-            -backed
-            tied up
-            zedong
-            bathing
-            jail
-            reticulum
-            rear end
-            sakimichan
-            behind bars
-            shirtless
-            sakimichan
-            seductive
-            sexi
-            sexualiz
+            lolita
+            making love
+            mammaries
+            massacre
+            meth
+            naked
+            negligee
+            nipple
+            no clothes
+            nude
+            organs
+            orgy
+            ovaries
+            penis
+            phallus
+            porn
+            rape
+            rear nude
+            rule34
+            scantily clad
+            see-through
             sexual
+            sexualized
+            shag
+            shibari
+            shit
+            skeletal gore
+            slaughter
+            slave
+            smut
+            sperm
+            suicide
+            tied up
+            torture
+            unclothed
+            vagina
+            veiny penis
+            violence
+            visceral
+            vivisection
+            vomit
+            wang
+            warts
+            wearing nothing
+            wincest
+            wound
+            zero clothes
             """;
 
         /// <summary>
